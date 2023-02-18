215f58f05f3bfb6ec184837cc05ece0f2c47595e
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-02-18 02:49:01 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const nftmarketaddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-export const nftaddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+export const nftmarketaddress = "0x9e9cE7082E4B1308Cb2186EfD28ea0ABcFcF4187"
+export const nftaddress = "0xe0718e419092311303ecC848152856a8f1a4d9F4"
